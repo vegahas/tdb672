f677ae6485e0eed9dc1f233c60cf615ec3f411a9
jennysj <je.sj@hotmail.com>
2016-03-10 12:45:06 +0100

    controller superclass, fixed login

--- src/GUI/createUserController.java
+++ src/GUI/createUserController.java
@@ -7,10 +7,10 @@ import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
-/**
- * Created by Jenny on 07.03.2016.
- */
-public class createUserController {
+    /**
+     * Created by Jenny on 07.03.2016.
+     */
+public class createUserController extends Controller{
 
     private MainStart main = new MainStart();
     @FXML
@@ -19,16 +19,34 @@ public class createUserController {
     private TextField userName;
     @FXML
     private Button confirmButton;
+
     @FXML
     private void back() throws IOException{
         main.showLoginScene();
     }
+
+
     @FXML
     private void createUser() throws IOException{
-        //String userID = something(userName.getText())
-        //userName.setText("Your userID is " + userID + ". Remember it.");
-        userName.setVisible(true);
-        confirmButton.setDisable(true);
+        int id = guiConnect.createUser(userName.getText());
+        if (id != -1) {
+            userIDLabel.setVisible(true);
+            userIDLabel.setText("Your userID is " + id + ". Remember it.");
+            confirmButton.setDisable(true);
+        }
+        else{
+            userIDLabel.setVisible(true);
+            userIDLabel.setText("Something is wrong, try another username");
+        }
+    }
 
+    public static void main(String[] args) {
+        createUserController cu = new createUserController();
+        cu.initialize();
+    }
+
+    private void initialize(){
+        userIDLabel.setVisible(false);
+        System.out.println("hei");
     }
 }
